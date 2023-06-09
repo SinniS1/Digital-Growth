eb9fb0a420481205cd0b838a2063b7d010c534b4
SinniS1 <sinniscode@gmail.com>
2023-06-10 02:21:39 +0530

    Component added

--- src/app/(pages)/about/page.tsx
+++ src/app/(pages)/about/page.tsx
@@ -1,7 +1,64 @@
 import React from 'react'
+import './AboutPage.css'
+import '../../components/FeatureSection/FeatureSection.css'
+import FeatureSection from '@/app/components/FeatureSection/FeatureSection'
 
 const page = () => {
-  return <div>About us</div>
+  return (
+    <main className='AboutPage'>
+      <div className='AboutPage__Landing'>
+        <span className='About__Heading'>About Us</span>
+        <p className='About__Para'>
+          We are startup of passionate developers, wants to revolutionize the small businesses with
+          our digital solutions.
+        </p>
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img src='./4.png' alt='About__Image' className='About__Image' />
+        <span className='About__Heading'>Our mission</span>
+        <p className='About__Para'>{`At DigitalGrowth , we believe in the transformative power of digital solutions. Our dedicated team of professionals is passionate about helping businesses succeed online. From designing visually stunning websites to developing innovative mobile apps and implementing targeted marketing campaigns, we empower our clients to reach their full potential in the digital world. Trust DigitalGrowth as your digital partner and embark on a journey of growth and success.`}</p>
+      </div>
+      <div className='About_Section2'>
+        <span className='About__Heading Section2__Heading'>{`Let's give your business online presence.`}</span>
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img src='./6.png' alt='Section2__Image' className='About__Image' />
+      </div>
+      <div className='About_Section3'>
+        <div className='Section3_Team'>
+          <span className='About__Heading'>Our Team</span>
+          <p className='About__Para'>{`Meet the peoples behind the magic`}</p>
+        </div>
+        <FeatureSection
+          heading={'Sachin Yadav'}
+          para={`Meet Sachin Yadav, a highly motivated and skilled web developer with a passion for creating engaging and user-friendly web applications. With a strong foundation in web technologies and responsive web design, Sachin brings a keen eye for detail and a collaborative mindset to every project.`}
+          btn1={'Contact Us'}
+          btn1Link={'/contact'}
+          image='./SachinProfile.jpg'
+          profileImage={true}
+          isLeftImage={true}
+          CardClassName='CardContainer'
+        />
+        <FeatureSection
+          heading={'Sukhvinder Singh'}
+          para={`Sukhvinder Singh, a highly motivated and skilled App developer with a passion for creating engaging and user-friendly mobile applications. With a strong foundation in App technologies . Sukhvinder brings a keen eye for detail and a collaborative mindset to every project.`}
+          btn1={'Contact Us'}
+          btn1Link={'/contact'}
+          image='./SachinProfile.jpg'
+          profileImage={true}
+          isLeftImage={false}
+          CardClassName='CardContainer'
+        />
+        <FeatureSection
+          heading={'Bharat Yadav'}
+          para={`Introducing Bharat Yadav, a skilled Digital Marketing Manager. With a strong focus on driving successful digital campaigns, Bharat brings expertise in leveraging various marketing channels, increasing brand visibility, and generating qualified leads. With a strategic mindset and a keen eye for market trends, Bharat is dedicated to achieving outstanding results in the digital landscape.`}
+          btn1={'Contact Us'}
+          btn1Link={'/contact'}
+          image='./SachinProfile.jpg'
+          profileImage={true}
+          isLeftImage={true}
+          CardClassName='CardContainer'
+        />
+      </div>
+    </main>
+  )
 }
-
 export default page
